09200d8fbb5e01183adc5c0ad608cff3a3fa2a4c
suckitfa <254202042@qq.com>
2021-09-23 13:10:17 +0800

    feat:初始化完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    title: 'Chen\'s blog',
-    description: '我的个人网站',
+    title: 'Guangyuan\'s blog',
+    description: '广园的个人博客',
     head: [ // 注入到当前页面的 HTML <head> 中的标签
-      ['link', { rel: 'icon', href: '/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+      ['link', { rel: 'icon', href: '/log.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     base: '/', // 这是部署到github相关的配置
     markdown: {
       lineNumbers: false // 代码块显示行号
     },
     themeConfig: {
+        theme:"vuepress-theme-reco",
       nav:[ // 导航栏配置
         {text: '前端基础', link: '/accumulate/' },
         {text: '算法题库', link: '/algorithm/'},
