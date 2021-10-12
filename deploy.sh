fc15b08c8b21e73b501538e156651970f23445f0
suckitfa <254202042@qq.com>
2021-10-12 08:15:06 +0800

    feat:更新一位同学的面试指南

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,3 @@ git push -f git@github.com:suckitfa/suckitfa.github.io.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
-
-cd -
