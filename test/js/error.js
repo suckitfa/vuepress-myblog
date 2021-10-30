e8d15a98f2c7f4a5cd63f739b99730323102c217
suckitfa <254202042@qq.com>
2021-10-30 17:42:25 +0800

    update:类型转换

--- test/js/error.js
+++ test/js/error.js
@@ -1,8 +1,23 @@
-function getType(val) {
-    return Object.prototype.toString.call(val)
-        .split(' ')
-        .pop().slice(0, -1).toLowerCase();
+function type(val) {
+    const result = Object.prototype.toString.call(val);
+    // 这个正则，扣出数据类型名字
+    return result.match(/\[object (.*?)\]/)[1].toLowerCase();
 }
+[
+    'String',
+    'Boolean',
+    'Number',
+    'Undefined',
+    'Null',
+    'Symbol',
+    'Boolean',
+    'Function',
+    'RegExp'
+].forEach(item => {
+    type['is' + item] = (val) => {
+        return type(val) === item.toLowerCase();
+    }
+})
 
-const res = getType(1)
-console.log(res)
+console.log(type(1))
+console.log(type.isNumber(NaN))
